b33aed750d53554fd581c44e9c0584634533c920
midoabdo14 <91239112+midoabdo14@users.noreply.github.com>
2021-10-27 11:56:12 +0900

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,4 +1,4 @@
-# %tensorflow_version 1.x
+# Import deepchem
 !wget -c https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
 !chmod +x Anaconda3-2019.10-Linux-x86_64.sh
 !bash ./Anaconda3-2019.10-Linux-x86_64.sh -b -f -p /usr/local
@@ -7,29 +7,33 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages/')
 import deepchem as dc
 
+# Import other libraries
 import numpy as np
 import random
 import tensorflow as tf
 np.random.seed(123)
 tf.random.set_random_seed(123)
-
 import pandas as pd
-dr=pd.read_csv('test.csv')
+
+# Explore data
+dr=pd.read_csv('/data/test.csv')
 dr.head()
 
+# Prepare data
 dr_tasks =['predict']
 featurizer = dc.feat.ConvMolFeaturizer()
-dataset_file = 'test.csv'
+dataset_file = '/data/test.csv'
 loader = dc.data.CSVLoader(
          tasks= dr_tasks,smiles_field="smiles", featurizer=featurizer) 
 rdtest = loader.featurize(dataset_file,shard_size=8192)
 
 from deepchem.models import GraphConvModel
 
-model= GraphConvModel(model_dir='/model',n_tasks=1)
+model= GraphConvModel(model_dir='/trained model',n_tasks=1)
 model.restore()
 
 prediction1=model.predict(rdtest)
 
-prediction=predcition.reshape(3897,2)
+# save model prediction
+# prediction=predcition.reshape(3897,2)
 pd.DataFrame(prediction).to_csv("result.csv", index = None)
