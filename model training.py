2f5bff936e5fd6d8c2f48d0aa338615f12568bcd
midoabdo14 <91239112+midoabdo14@users.noreply.github.com>
2021-10-27 11:46:48 +0900

    Update model training.py

--- model training.py
+++ model training.py
@@ -7,6 +7,7 @@ import sys
 sys.path.append('/usr/local/lib/python3.7/site-packages/')
 import deepchem as dc
 
+# Import other libraries
 import numpy as np
 import random
 import tensorflow as tf
@@ -14,18 +15,19 @@ import pandas as pd
 np.random.seed(123)
 tf.random.set_random_seed(123)
 
-# Read datafile 
+# Explore datafile 
 file=pd.read_csv('data/covid.csv')
+file.head()
+
+# Prepare data
 file_tasks =['activity']
-
-
-
 featurizer = dc.feat.ConvMolFeaturizer()
 dataset_file = 'data/covid.csv'
 loader = dc.data.CSVLoader(
         tasks= file_tasks, smiles_field="smiles", featurizer=featurizer) 
 dataset = loader.featurize(dataset_file,shard_size=8192)
 
+# Data normalization
 transformers= [
                dc.trans.BalancingTransformer(
                 transform_w=True, dataset=dataset
@@ -35,6 +37,7 @@ transformers= [
 for transformer in transformers:
   dataset= transformer.transform(dataset)
 
+# Data splitting
 splitter=dc.splits.RandomSplitter(dataset)
 split_datas = splitter.k_fold_split(dataset,5)
 
@@ -42,12 +45,12 @@ from deepchem.models import GraphConvModel
 metric = dc.metrics.Metric(
     dc.metrics.roc_auc_score, np.mean, mode="classification")
 
-# conduct cross-validation
+# Cross-validation
 validation_scores = []
 train_scores = []
 learning_rate = dc.models.optimizers.ExponentialDecay(0.0002, 0.9, 1000)
 for train_set, val_set in split_datas:
-    model = dc.models.GraphConvModel(len(file_tasks),batch_size =64,droupout=0,dense_layer_size=256,learning_rate=learning_rate,model_dir='model5')
+    model = dc.models.GraphConvModel(len(file_tasks),batch_size =64,droupout=0,dense_layer_size=256,learning_rate=learning_rate,model_dir='trained model')
     callback = dc.models.ValidationCallback(val_set, 1000, metric)
     model.fit(train_set, nb_epoch = 20,callbacks=callback)
     train_score = model.evaluate(train_set, [metric], transformers)
