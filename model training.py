4baccd6452c1554b3418b62ee0d4e0e9d3d3dea8
midoabdo14 <91239112+midoabdo14@users.noreply.github.com>
2021-10-29 13:27:55 +0900

    Update model training.py

--- model training.py
+++ model training.py
@@ -22,7 +22,7 @@ file.head()
 # Prepare data
 file_tasks =['activity']
 featurizer = dc.feat.ConvMolFeaturizer()
-dataset_file = 'data/covid.csv'  # you can replace this with your own data
+dataset_file = 'data/covid.csv'  # you can replace this path with your own data file path
 loader = dc.data.CSVLoader(
         tasks= file_tasks, smiles_field="smiles", featurizer=featurizer) 
 dataset = loader.featurize(dataset_file,shard_size=8192)
